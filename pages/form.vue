d2f3e78d28c587f4191e834beed29a5235924dcd
Daiki Kunii <dai0106ki@gmail.com>
2019-08-13 03:59:12 +0900

    [fix] form

--- pages/form.vue
+++ pages/form.vue
@@ -4,7 +4,9 @@
         <div class="shape shape-style-1">
             <img src="/toryumon/back2.png" width="100%" height="90%" >
         </div>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfFshr4USznOSpgRAzLV238cbyA-jk4f7JAPWQ0x6rAl5nQGw/viewform?embedded=true" width="100%" height="8000" frameborder="0" marginheight="0" marginwidth="0" class="mt-lg mb-5">読み込んでいます…</iframe>
+        <form>
+        　<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfFshr4USznOSpgRAzLV238cbyA-jk4f7JAPWQ0x6rAl5nQGw/viewform?embedded=true" width="100%" height="8000" frameborder="0" marginheight="0" marginwidth="0" class="mt-lg mb-5">読み込んでいます…</iframe>
+        </form>
         <div style="text-align: center;">
             <img src="/toryumon/logo4.png" alt="Raised circle image" width="90%" height="90%" style="text-align: center; margin-top:-800px;">
         </div>
