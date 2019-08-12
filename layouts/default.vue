ff2e268dc232bce841701fa66560a25cb69abc62
Daiki Kunii <dai0106ki@gmail.com>
2019-08-12 21:32:33 +0900

    [fix] GA

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,15 +14,6 @@
     </div>
 </template>
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-145396788-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-145396788-1');
-</script>
 <script>
 import StarterHeader from "~/components/argon-layout/starter/StarterHeader";
 import StarterFooter from "~/components/argon-layout/starter/StarterFooter";
